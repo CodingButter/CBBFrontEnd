6fba8d0a5b8c19b9768be5d993a4918f518a2015
Jamie Nichols <jamie337nichols@gmail.com>
2020-08-24 21:51:34 -0400

    Updated the React paths paths fixed the issue with vscode making react paths reletive to the node_module folder instead of a reference as a module

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from "react"
 import { Router } from "@reach/router"
-import Chat from "./components/Chat"
-import Drop from "./components/Drop"
-import Events from "./components/Events"
+import Chat from "./Components/Chat"
+import Drop from "./Components/Drop"
+import Events from "./Components/Events"
 import io from "socket.io-client"
 
 const ENDPOINT = "http://107.4.77.230:8080" // TODO Move ENPOINT to the .env file
-const socket = io(ENDPOINT)
+const socket = io(ENDPOINT,{secure: false})
 
 function App() {
     return (
         <Router>
             <Chat path="/chat" socket={socket} />
             <Drop path="/drop" socket={socket} />
+            <Events path="/events" socket={socket}/>
         </Router>
     )
 }
---
+++ src/Components/Chat/Chat.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react"
+import Message from "./Message"
+import FilterInput from "./FilterInput"
+import { ViewCounter } from "../Widgets"
+
+export default ({ socket }) => {
+    console.log(socket)
+    const [messages, setMessages] = useState([])
+    const [messageFilter, setMessageFilter] = useState("")
+
+    useEffect(() => {
+        socket.on("newmessage", data => {
+            console.log(data.tags)
+            setMessages(oldMessages => [...oldMessages, data])
+        })
+    }, [socket])
+
+    return (
+        <div id="widget-wrapper">
+            <ViewCounter socket={socket} />
+            <FilterInput setFilter={setMessageFilter} />
+            <div className="chat-box">
+                {messages &&
+                    messages
+                        .filter(message => {
+                            const matches = message.message.match(
+                                new RegExp(messageFilter, "g")
+                            )
+                            return matches
+                        })
+                        .reverse()
+                        .map(({ tags, message }) => {
+                            return (
+                                <Message
+                                    tags={tags}
+                                    message={message}
+                                    key={tags.id}
+                                />
+                            )
+                        })}
+            </div>
+        </div>
+    )
+}
